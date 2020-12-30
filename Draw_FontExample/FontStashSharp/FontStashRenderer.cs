afad247257b5b1eaf45c868b33730e3a278d415f
AlzPatz <alexpatersonsmail@gmail.com>
2020-12-30 12:58:23 +0000

    Implements FontStashSharp example using existing Yak2D DrawStage Rendering

---
+++ Draw_FontExample/FontStashSharp/FontStashRenderer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+using Yak2D;
+using FontStashSharp.Interfaces;
+
+namespace Draw_FontExample
+{
+    public class FontStashRenderer : IFontStashRenderer
+    {
+        private IDrawing _drawService;
+        private IDrawStage _drawStage;
+
+        private CoordinateSpace _coordinateSpace;
+        private float _screenHeight;
+
+        public FontStashRenderer(int screenHeight)
+        {
+            _screenHeight = (float)screenHeight;
+            _coordinateSpace = CoordinateSpace.Screen; //Must be coordinate space as FontStashSharp requires positive Y to be negative
+        }
+
+        public void SetYakDrawingObjects(IDrawStage drawStage, IDrawing draw)
+        {
+            _drawStage = drawStage;
+            _drawService = draw;
+        }
+
+        public void SetCameraYResolutionForTransform(float screenHeight)
+        {
+            _screenHeight = screenHeight;
+        }
+
+        public void Draw(ITexture2D texture,
+                         PointF position,
+                         Rectangle? sourceRectangle,
+                         System.Drawing.Color color,
+                         float rotation,
+                         PointF origin,
+                         PointF scale,
+                         float depth)
+        {
+            //I've assumed that these paramaters match the function of those that would be used in a "spritebatch" implementation (such as XNA)
+
+            if (_drawStage == null)
+            {
+                throw new NullReferenceException("No Yak2D DrawStage has been set in FontStashRenderer");
+            }
+
+            if (_drawService == null)
+            {
+                throw new NullReferenceException("No Yak2D Drawing Service has been set in FontStashRenderer");
+            }
+
+            //Generate Colour in Yak2D format (going from 4 bytes (ints 0 to 255) to 4 floats 0 to 1
+            var colour = new Colour(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
+
+            //Generate Texture Coordinates from source Rectangle. Source rectangle is in Texels, required Yak2D coordinates are fractional
+
+            //We need to know entire size of texture we are dealing with
+            //We assume here a cast from Interface to implementation. Not good practice, but suitable for example.
+            var textureObject = texture as Texture2D;
+            var textureSize = _drawService.GetSurfaceDimensions(textureObject.YakTexture);
+
+            //Set as if whole texture first (Yak2D texture coordinates are origin top left)
+            var minx = 0.0f;
+            var maxx = 1.0f;
+            var miny = 0.0f;
+            var maxy = 1.0f;
+
+            var unscaledWidth = textureSize.Width;
+            var unscaledHeight = textureSize.Height;
+
+            if (sourceRectangle.HasValue)
+            {
+                var rect = sourceRectangle.Value; //System.Drawing.Rectangle is also origin top left (x, y)
+
+                minx = (float)rect.X / (float)textureSize.Width;
+                maxx = (float)(rect.X + rect.Width) / (float)textureSize.Width;
+
+                miny = (float)rect.Y / (float)textureSize.Height;
+                maxy = (float)(rect.Y + rect.Height) / (float)textureSize.Height;
+
+                unscaledWidth = rect.Width;
+                unscaledHeight = rect.Height;
+            }
+
+            //Position in Yak2D helper function is the centre of the quad. Position in SpriteBatch is related to the origin set for the sprite, rotation and the position given
+            //We must transform the position
+
+            var centreUnscaled = new Vector2(unscaledWidth, unscaledHeight) * 0.5f;
+            var originVector = new Vector2(origin.X, origin.Y);
+
+            var originToQuadCentre = centreUnscaled - originVector;
+
+            var originPosition = new Vector2(position.X, position.Y);
+
+            if (originToQuadCentre != Vector2.Zero)
+            {
+                //Spritebatch rotates clockwise, this is a negative rotation using Vector2 transform matrices
+                originToQuadCentre = Vector2.Transform(originToQuadCentre, Matrix3x2.CreateRotation(-rotation));
+            }
+
+            var quadPosition = originPosition + originToQuadCentre;
+            
+            //HERE is where we account for the coordinate system differences
+            //Convert Y position from "origin top left" to "origin middle of screen and positive Y towards top of screen"
+            var midY = _screenHeight * 0.5f;
+            var distanceFromMidY = midY - quadPosition.Y;
+            quadPosition.Y = distanceFromMidY;
+
+            var width = unscaledWidth * scale.X;
+            var height = unscaledHeight * scale.Y;
+
+            _drawService.Helpers.DrawTexturedQuad(_drawStage,
+                                                  _coordinateSpace,
+                                                  textureObject.YakTexture,
+                                                  colour,
+                                                  quadPosition,
+                                                  width,
+                                                  height,
+                                                  depth,
+                                                  0,
+                                                  rotation,
+                                                  minx,
+                                                  miny,
+                                                  maxx,
+                                                  maxy,
+                                                  TextureCoordinateMode.Wrap);
+        }
+    }
+}
---
+++ Draw_FontExample/FontStashSharp/Texture2D.cs
@@ -0,0 +1,97 @@
+﻿using FontStashSharp.Interfaces;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using Yak2D;
+
+namespace Draw_FontExample
+{
+    public class Texture2D : ITexture2D
+    {
+        private ISurfaces _surfaces;
+        private uint _width;
+        private uint _height;
+
+        private ITexture _texture;
+        private Vector4[] _pixelData;
+
+        public Texture2D(ISurfaces surfaces, int width, int height)
+        {
+            if (surfaces == null)
+            {
+                throw new NullReferenceException("The Yak2D Surfaces service reference is NULL. Do not pass a NULL service");
+            }
+
+            if (width < 1 || height < 1)
+            {
+                throw new ArgumentException("Trying to create a texture with dimension 0 or negative, doesn't make sense");
+            }
+
+            _surfaces = surfaces;
+            _width = (uint)width;
+            _height = (uint)height;
+
+            //Fill pixel data array with clear pixels
+            var numberOfPixels = _width * _height;
+            _pixelData = Enumerable.Repeat<Vector4>(Vector4.Zero, (int)numberOfPixels).ToArray();
+        }
+
+        public ITexture YakTexture => ReturnTexture();
+
+        private ITexture ReturnTexture()
+        {
+            if (_texture == null)
+            {
+                CreateTexture();
+            }
+
+            return _texture;
+        }
+
+        private void CreateTexture()
+        {
+            if (_texture != null)
+            {
+                _surfaces.DestroySurface(_texture);
+            }
+
+            _texture = _surfaces.CreateRgbaFromData((uint)_width, (uint)_height, _pixelData);
+        }
+
+        public void Dispose()
+        {
+            _surfaces.DestroySurface(_texture);
+        }
+
+        public void SetData(Rectangle bounds, byte[] data)
+        {
+            //This implementation just creates a new texture on each setdata call
+            //I may expose updating part of a texture in Yak2D to save on data transfer for these types of operations
+
+            //Update the data array
+            //The byte array has 4 bytes R, G, B, A for every pixel
+
+            var updateDataCount = 0;
+            for (var y = bounds.Y; y < bounds.Y + bounds.Height; y++)
+            {
+                for (var x = bounds.X; x < bounds.X + bounds.Width; x++)
+                {
+                    var textureDataLinearIndex = (y * _width) + x;
+
+                    var r = updateDataCount;
+                    var g = r + 1;
+                    var b = g + 1;
+                    var a = b + 1;
+
+                    _pixelData[textureDataLinearIndex] = new Vector4(((int)data[r]) / 255.0f, ((int)data[g]) / 255.0f, ((int)data[b]) / 255.0f, ((int)data[a]) / 255.0f);
+
+                    updateDataCount += 4;
+                }
+            }
+
+            //Recreate Texture
+            CreateTexture();
+        }
+    }
+}
