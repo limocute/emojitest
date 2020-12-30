26bc41245613f4e5e0a30c4b7b5d2712eae6fb92
limocute <limocute@limocute.com>
2020-12-30 09:48:42 +0800

    init

---
+++ Draw_FontExample/DrawFontExample.cs
@@ -0,0 +1,70 @@
+﻿using SampleBase;
+using System.Numerics;
+using Yak2D;
+
+namespace Draw_FontExample
+{
+    /// <summary>
+    /// Bitmap Font text rendering example
+    /// </summary>
+    public class DrawFontExample : ApplicationBase
+    {
+        private IDrawStage _drawStage;
+        private ICamera2D _camera;
+
+        public override string ReturnWindowTitle()
+        {
+           
+            return "Drawing Bitmap Fonts";
+        }
+
+        public override void OnStartup() {
+         
+        
+        }
+
+        public override bool CreateResources(IServices yak)
+        {
+            _drawStage = yak.Stages.CreateDrawStage();
+
+            _camera = yak.Cameras.CreateCamera2D(960, 540, 1.0f);
+
+           
+            return true;
+        }
+
+        public override bool Update_(IServices yak, float timeSinceLastUpdateSeconds) => true;
+        public override void PreDrawing(IServices yak, float timeSinceLastDrawSeconds, float timeSinceLastUpdateSeconds) { }
+
+        public override void Drawing(IDrawing draw,
+                                     IFps fps,
+                                     IInput input,
+                                     ICoordinateTransforms transforms,
+                                     float timeSinceLastDrawSeconds,
+                                     float timeSinceLastUpdateSeconds)
+        {
+           
+            draw.DrawString(_drawStage,
+                             CoordinateSpace.Screen,
+                             "Test Emoji 🎄😃😜🙉😉😍😝🙅🙊✂✈✊🚄🚑🆗♻⚽🍚🎄",
+                             Colour.IndianRed,
+                             18.0f,
+                             new Vector2(-430.0f, 220.0f),
+                             TextJustify.Left,
+                             0.5f,
+                             0);
+
+           
+        }
+
+        public override void Rendering(IRenderQueue q, IRenderTarget windowRenderTarget)
+        {
+            q.ClearColour(windowRenderTarget, Colour.Clear);
+            q.ClearDepth(windowRenderTarget);
+            q.Draw(_drawStage, _camera, windowRenderTarget);
+        }
+
+        public override void Shutdown() { }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# emojitest
+
+Hello, this project is used to test yak2d drawing Emoji characters, please open the solution draw_FontExample.sln 
+
+The Emoji string used for testing is shown in the_ In the drawing method of fontexample.
+
+# Segoe UI Emoji.ttf
+Project root directory Segoe UI Emoji.ttf  The file is Emoji font of windows 10 system
